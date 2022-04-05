5e4d82dd26a0dbc5e67d2abf6a84292808830fa1
no92one <linus.lindroth.92@gmail.com>
2022-04-05 07:52:17 +0200

    adding the start of spring boot project

---
+++ src/main/java/com/OOP2PG1/Oop2Pg1Application.java
@@ -0,0 +1,13 @@
+package com.OOP2PG1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oop2Pg1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oop2Pg1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/OOP2PG1/Oop2Pg1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OOP2PG1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oop2Pg1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
