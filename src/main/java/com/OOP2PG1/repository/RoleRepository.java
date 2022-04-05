4a710fbe54ed18543f7797ad715d26645ff5ecc5
Liha86 <sanjin.elfynd@gmail.com>
2022-04-05 17:11:32 +0200

    Created repository + RoleRepository

---
+++ src/main/java/com/OOP2PG1/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.OOP2PG1.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends MongoRepository<Role, String> {
+    Optional<Role> findByName(ERole name);
+}
