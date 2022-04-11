837c2c8a00a671c42d019c981c66abb3ddb86193
qoentz <englund.jonatan@gmail.com>
2022-04-11 23:40:58 +0200

    testing

--- src/main/resources/public/page-components/createSite.js
+++ src/main/resources/public/page-components/createSite.js
@@ -2,19 +2,29 @@ class CreateSite extends Component{
     
     get template(){
     return `
-       
-            <div class="createsite-box">
-                <h1>Create Site</h1>
+
+       <h1>Create Site</h1>
+            <div class="createsite-block">
                 <form id="createsite">
                     <label>Site name</label>
                     <input type="text" id="register-username" placeholder="">
                     <label>Title</label>
                     <input type="email" id="register-email" placeholder="">
                     <label>Description</label>
-                    <input type="password" id="register-password" placeholder="">
-                    <input type="submit" class="Submit" value="Submit">
-                </form>
+                    <input type="password" id="register-password" placeholder=""
+                    </form>
             </div>
+            <div class="createsite-block2">
+                <form>
+                    <label>Color Theme</label>
+                    <input type="text" id="register-username" placeholder="">
+                    <label>Title</label>
+                    <input type="email" id="register-email" placeholder="">
+                    <label>Description</label>
+                    <input type="password" id="register-password" placeholder="">
+                    <input type="submit" class="Create" value="Submit">
+                    </form>
+                
             
         `
 
