32d941c7ac3e178068b415d6b9551a869c2036d3
qoentz <englund.jonatan@gmail.com>
2022-04-12 10:33:37 +0200

    uh

--- src/main/resources/public/page-components/createSite.js
+++ src/main/resources/public/page-components/createSite.js
@@ -25,9 +25,6 @@ class CreateSite extends Component{
         let result = await fetch(apiHost + '/api/auth/signout', {
             method: 'delete',
         })
-        // if(result.status != 200){
-        //     location.hash = "login"
-        // }
         let data = await result.json()
         console.log(result, data)
         if(result.status === 200){
@@ -39,10 +36,11 @@ class CreateSite extends Component{
     return `
 
        <h1>Create Site</h1>
+       <form id="logout" method="delete">
+       <input type="submit" class="Submit" value="Logout"/>
+       <input type="submit" class="Submit" value="Main page"/>
+       </form>
             <div class="createsite-block">
-            <form id="logout">
-                <input type="submit" class="Submit" value="Logout"/>
-                </form>
 
                 <form id="createsite">
                     <label>Site name</label>
@@ -50,18 +48,11 @@ class CreateSite extends Component{
                     <label>Title</label>
                     <input type="email" id="title" placeholder="">
                     <label>Description</label>
-                    <input type="password" id="register-password" placeholder=""
-                    </form>
-            </div>
-            <div class="createsite-block2">
-                <form>
-                    <label>Color Theme</label>
-                    <input type="text" id="register-username" placeholder="">
-                    <label>Title</label>
-                    <input type="email" id="register-email" placeholder="">
-                    <label>Description</label>
-                    <input type="password" id="register-password" placeholder="">
-                    <input type="submit" class="Create" value="Submit">
+                    <input type="text" id="description" placeholder=""
+                    <label>Upload logo</label>
+                    <label>Upload wallpaper</label>
+                    <input type="submit" class="Submit" value="Create">
+                    </div>
                     </form>
                 
             
