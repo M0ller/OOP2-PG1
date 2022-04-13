b7ebcc6304d3fdf536477d70259631ac0f01f2d2
no92one <linus.lindroth.92@gmail.com>
2022-04-13 10:47:21 +0200

    update load()

--- src/main/resources/public/page-components/sitefirstpage.js
+++ src/main/resources/public/page-components/sitefirstpage.js
@@ -1,44 +1,19 @@
 class Sitefirstpage extends Component{
+
+    data = {}
    
-     async getSite(){
-        console.log("Before")
-        let result = await fetch('http://localhost:8080/site/book').then(response =>response.json()) //.then(data => console.log(data))
-        let data = await result
-        site = data
-        title = data.title
-        console.log("After")
-        /* let result = await fetch(apiHost + '/site/book', {
-            method: 'get'
-        })
-        //console.log(result, data) */      
-        console.log("result")
+     async load(){
+        let result = await fetch('http://localhost:8080/site/${urlHeader}')
 
-        console.log(data)
-        console.log(title)
-        return title
+        this.data = await result.json()
+        
     } 
+
     
-     template =`${this.getSite()} 
-     <h1>You got Stuff! ${title}</h1>`
-    
-
-/*     async getSite(){
-     await fetch( apiHost + `/site/book`, {
-        method: "GET", 
-        headers: {'Content-Type': 'application/json'},
-        //body: undefined // string, FormData, Blob, BufferSource, or URLSearchParams
-        //referrer: "about:client", // or "" to send no Referer header,
-        // or an url from the current origin
-
-        //redirect: "follow", // manual, error
-        })
-    }
-    getSite() */
-
-/*     async returnSite(){
-        return await fetch(apiHost + `/site/book`); 
-    }
-
-    returnSite() */
-    
+     get template() {
+        return `
+        <h1>You got Stuff! ${this.data.title}</h1>
+ 
+        `
+     }
 }
