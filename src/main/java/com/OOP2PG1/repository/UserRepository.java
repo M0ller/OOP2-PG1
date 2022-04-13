f2d29b3391b7121bd9bb3a1dcf648d611c55d3bf
simon M <simon.mollerberg@outlook.com>
2022-04-13 19:07:46 +0200

    stuff

--- src/main/java/com/OOP2PG1/repository/UserRepository.java
+++ src/main/java/com/OOP2PG1/repository/UserRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
+
     Optional<User> findByUsername(String username);
+
+
 
     Boolean existsByUsername(String username);
 
