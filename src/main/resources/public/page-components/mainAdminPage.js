8ab8d7c41608e171df161206c02dd5ae60683f63
Liha86 <sanjin.elfynd@gmail.com>
2022-04-13 13:41:03 +0200

    !

--- src/main/resources/public/page-components/mainAdminPage.js
+++ src/main/resources/public/page-components/mainAdminPage.js
@@ -1,26 +1,29 @@
 class MainAdminPage extends Component{
 
     events(){
-        $('body').on('submit', '#mainAdminPage', this.loadMain)
+        $('body').on('submit', '#mainAdminPage', this.load)
         $('body').on('submit', '#logout', this.logout)
     }
 
-  async loadMain(){
-        /*let result = await fetch(apiHost + '/api/default/main')    
+    siteTitle = []
+
+  async load(){
+        console.log(user.username)
+        let result = await fetch(apiHost + '/site/get/' + user.username)    
         let data = await result.json()
-        console.log(result, data);
+
         for(let i = 0; i<data.length; i++){
-            data[i] = new Sitesdata( data[i] ).render()
+            data[i] = new this.siteTitle ( data[i] ).render()
+            //this.siteTitle.push(data[i].title)
         }
         this.data = data  
         //$('footer').html(this.data.join('<hr>')) 
-        */
-       return "SSS"     
+
     }
 
     async logout(event){
         event.preventDefault()
-        let result = await fetch(apiHost + '/api/auth/signout', {
+        let result = await fetch(apiHost + '/site/auth/signout', {
             method: 'delete',
         })
         let data = await result.json()
@@ -39,12 +42,18 @@ class MainAdminPage extends Component{
         
         <input type="edit" class="CreateSite" value="Create Site">
         <input type="edit" class="CreateSite" value="Logout">
+        <ul id="siteTitle"></ul>
+        <section>
+                ${this.data.join('')}
+            </section>
 
+        
         <div class="dropdown">
             <span>Show list of sites</span>
              <div class="dropdown-content">
             <p><a href="#">Barcat master</a> </p>
             <p><a href="#">D.Book</a> </p>
+           
            
             
 
